71e0111b6559dbfa261bc24cf3729e54db358d23
Guilherme Bernal <82238770+Guilherme-Bernal@users.noreply.github.com>
2022-08-19 11:10:48 -0300

    Add files via upload

---
+++ Programação orientada/Primeira/src/App.java
@@ -0,0 +1,5 @@
+public class app {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Resultado 2+2 =" + (2 + 2));
+    }
+}
---
+++ Programação orientada/primeira_1/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Resultado entre 2 + 2 = " + (2 + 2));
+        int numero;
+        String Texto = "Ola";
+        numero = 10;
+        System.out.println("resultado 2+2=" + (2 + 2));
+        System.out.println("O valor do numero = " + numero);
+        System.out.println("Ola mundo");
+        System.out.println(Texto);
+    }
+}
